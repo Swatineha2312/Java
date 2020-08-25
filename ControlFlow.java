c92ca05b31e9f83064af7f3ae1bc77995de9ac61
Swatineha2312 <swatineha2312@gmail.com>
2020-08-25 17:55:30 +1000

    control flow statements

---
+++ ControlFlow.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class ControlFlow{
+    public static void main(String[] args) {
+        /*
+    //Comparison Operators
+        int x=1, y=1;
+        x==y;
+
+     //Logical Operators
+      &&, ||, !
+
+     //If statements
+        if(temp>30){ syso Drink water}
+        else if(temp<20){ syso Beautiful day)
+        else syso Its cold
+
+    //More on if
+    int income=120_000;
+    boolean hasHighIncome=(income>100_000);
+
+    // Ternary operator
+    int income=120_000;
+    String className=(income>100_000)?"first":"economy";
+
+    //Switch
+    String role="Developer"
+    switch(role):
+    case "Developer":
+    u r developer
+    break;
+    case "Engineer"
+    u r engineer
+    break;
+    default:
+    u r guest
+
+        */
+    }
+}
