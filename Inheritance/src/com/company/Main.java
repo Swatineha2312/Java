ac8f12944ff5847d152f824fe1ed948b5139d6cc
Swatineha2312 <swatineha2312@gmail.com>
2020-09-30 15:34:10 +1000

    Inheritance with method overriding

---
+++ Inheritance/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var textBox= new TextBox();
+        textBox.setText("Inheritance");
+        System.out.println(textBox);
+    }
+}
